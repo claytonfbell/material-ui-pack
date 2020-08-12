c8c32d85001d6762ef9c13db6361d5b3b511cd20
Clay Bell <clay.bell@etix.com>
2020-08-11 18:15:38 -0700

    allow negative currency input

--- src/CurrencyField.tsx
+++ src/CurrencyField.tsx
@@ -8,15 +8,23 @@ interface CurrencyFieldProps {
   label?: string
   disabled?: boolean
   required?: boolean
+  allowNegative?: boolean
 }
 export default function CurrencyField(props: CurrencyFieldProps) {
   const { setValue } = useForm()
   const fmt = (s: string) =>
     s
-      .replace(/[^0-9.]/g, "")
+      .replace(/[^0-9.-]/g, "")
       .replace(/[.]+/g, ".")
-      .replace(/^([0-9]+)(\.[0-9]{1,2}).*/g, "$1$2")
+      .replace(/[-]+/g, "-")
+      .replace(/^(-?[0-9]+)(\.[0-9]{1,2}).*/g, "$1$2")
   const formatBlur = (s: string) => {
+    if (s.length > 2) {
+      s = s.substr(0, 1) + s.substr(1, s.length - 1).replace(/-/g, "")
+    }
+    if (!props.allowNegative) {
+        s = s.replace(/-/g, '')
+    }
     return Number(fmt(s)).toFixed(2)
   }
   return (
