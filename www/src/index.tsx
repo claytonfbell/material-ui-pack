b542c286e22431550b59e2568cfde4437a121703
Clay Bell <clay.bell@etix.com>
2021-10-30 10:35:34 -0700

    chore: supporting ie11 on the website until June 2022

--- www/src/index.tsx
+++ www/src/index.tsx
@@ -1,3 +1,7 @@
+// polyfills must be at the top!
+import "react-app-polyfill/ie11"
+import "react-app-polyfill/stable"
+
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
