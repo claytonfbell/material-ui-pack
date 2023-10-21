e96a04e183997015446ab5634a16aab23fb202a6
Clay Bell <clay.bell@etix.com>
2023-10-21 09:22:04 -0700

    fix: cleanup TextFieldBase component

--- src/TextFieldBase.tsx
+++ src/TextFieldBase.tsx
@@ -1,24 +1,18 @@
-import MUITextField, {
-  TextFieldProps as MUITextFieldProps,
-} from "@mui/material/TextField"
-import startCase from "lodash/startCase"
-import React from "react"
 import PhoneIcon from "@mui/icons-material/Phone"
 import Visibility from "@mui/icons-material/Visibility"
 import VisibilityOff from "@mui/icons-material/VisibilityOff"
-import InputAdornment from "@mui/material/InputAdornment"
 import IconButton from "@mui/material/IconButton"
+import InputAdornment from "@mui/material/InputAdornment"
+import TextField, { TextFieldProps } from "@mui/material/TextField"
+import startCase from "lodash/startCase"
+import React, { useEffect } from "react"
 
 type OnChange = (value: string) => void
 
 type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
-export type TextFieldBaseProps = Omit<
-  MUITextFieldProps,
-  "value" | "onChange"
-> & {
-  name?: string
-  value?: string
-  onChange?: OnChange
+export type TextFieldBaseProps = Omit<TextFieldProps, "value" | "onChange"> & {
+  value: string
+  onChange: OnChange
   formatter?:
     | StringFormatter
     | "capitalize"
@@ -27,100 +21,15 @@ export type TextFieldBaseProps = Omit<
     | "newPassword"
     | "password"
     | "email"
-  /**
-   * @deprecated - use formatter
-   */
-  capitalize?: boolean
-  /**
-   * @deprecated - use formatter
-   */
-  lowercase?: boolean
-  /**
-   * @deprecated - use formatter
-   */
-  phone?: boolean
-  /**
-   * @deprecated - use formatter
-   */
-  newPassword?: boolean
-  /**
-   * @deprecated - use formatter
-   */
-  password?: boolean
 }
 
 type StringFormatter = (str: string) => string
-
-const formatters = {
-  capitalize: (str: string) =>
-    str
-      .split(" ")
-      .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
-      .join(" "),
-  lowercase: (str: string) => str.toLowerCase(),
-  phone: (str: string) =>
-    str
-      .replace(/[^0-9() -]/g, "")
-      .replace(/^[^0-9]*([0-9]{3})[^0-9]*([0-9]{1,2})[^0-9]*$/g, "($1) $2")
-      .replace(/^[^0-9]*([0-9]{3})[^0-9]*([0-9]{1,3})$/g, "($1) $2")
-      .replace(
-        /^[^0-9]*([0-9]{3})[^0-9]*([0-9]{3})[^0-9]*([0-9]{1,4}).*$/g,
-        "($1) $2-$3"
-      ),
-  phoneBlur: (v: string) => {
-    if (v !== null) {
-      v = v.replace(/[^0-9]/g, "")
-      v = v.length > 10 ? v.substr(0, 10) : v
-      v =
-        v.length === 10
-          ? `(${v.substr(0, 3)}) ${v.substr(3, 3)}-${v.substr(6, 4)}`
-          : v
-    }
-    return v
-  },
-  email: (v: string) => {
-    const [user, host] = v.toLowerCase().substring(0, 320).split(/@/)
-    return `${user.trim()}${host !== undefined ? `@${host.trim()}` : ""}`
-  },
-}
 
 export const TextFieldBase = React.forwardRef<
   HTMLDivElement,
   TextFieldBaseProps
 >((originalProps, ref) => {
-  let {
-    formatter,
-    value: propsValue,
-    onChange: propsOnChange,
-    size = "small",
-    capitalize,
-    lowercase,
-    phone,
-    newPassword,
-    password,
-    ...props
-  } = originalProps
-
-  /**
-   * @deprecated props
-   */
-  if (formatter === undefined) {
-    if (capitalize) {
-      formatter = "capitalize"
-    }
-    if (lowercase) {
-      formatter = "lowercase"
-    }
-    if (phone) {
-      formatter = "phone"
-    }
-    if (newPassword) {
-      formatter = "newPassword"
-    }
-    if (password) {
-      formatter = "password"
-    }
-  }
+  const { formatter, value, onChange, ...props } = originalProps
 
   // handleChange
   function handleChange(
@@ -145,7 +54,7 @@ export const TextFieldBase = React.forwardRef<
     if (typeof formatter === "function") {
       newValue = formatter(newValue)
     }
-    onChange !== undefined && onChange(newValue)
+    onChange(newValue)
   }
 
   // handleBlur
@@ -156,14 +65,12 @@ export const TextFieldBase = React.forwardRef<
     if (formatter === "phone") {
       newValue = formatters.phoneBlur(newValue)
     }
-    onChange !== undefined && onChange(newValue)
-  }
+    onChange(newValue)
 
-  // manage state if no value and onChange
-  const [state, setState] = React.useState("")
-  const value = propsValue !== undefined ? propsValue : state
-  const onChange: OnChange =
-    propsOnChange !== undefined ? propsOnChange : (x) => setState(x)
+    if (props.onBlur !== undefined) {
+      props.onBlur(e)
+    }
+  }
 
   // password state
   const [error, setError] = React.useState(false)
@@ -229,7 +136,7 @@ export const TextFieldBase = React.forwardRef<
   props.fullWidth = props.fullWidth === undefined ? true : props.fullWidth
 
   // error state
-  React.useEffect(() => {
+  useEffect(() => {
     const errs = []
     // check for strong password
     if (formatter === "newPassword") {
@@ -261,21 +168,55 @@ export const TextFieldBase = React.forwardRef<
   }, [value, formatter])
 
   return (
-    <MUITextField
+    <TextField
+      {...props}
       ref={ref}
       value={value}
       onBlur={handleBlur}
       onChange={handleChange}
-      size={size}
-      inputProps={
-        formatter === "phone"
+      inputProps={{
+        ...props.inputProps,
+        ...(formatter === "phone"
           ? {
               pattern: "[0-9]*",
               step: "0.01",
             }
-          : undefined
-      }
-      {...props}
+          : undefined),
+      }}
     />
   )
 })
+
+// formatter functions
+const formatters = {
+  capitalize: (str: string) =>
+    str
+      .split(" ")
+      .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+      .join(" "),
+  lowercase: (str: string) => str.toLowerCase(),
+  phone: (str: string) =>
+    str
+      .replace(/[^0-9() -]/g, "")
+      .replace(/^[^0-9]*([0-9]{3})[^0-9]*([0-9]{1,2})[^0-9]*$/g, "($1) $2")
+      .replace(/^[^0-9]*([0-9]{3})[^0-9]*([0-9]{1,3})$/g, "($1) $2")
+      .replace(
+        /^[^0-9]*([0-9]{3})[^0-9]*([0-9]{3})[^0-9]*([0-9]{1,4}).*$/g,
+        "($1) $2-$3"
+      ),
+  phoneBlur: (v: string) => {
+    if (v !== null) {
+      v = v.replace(/[^0-9]/g, "")
+      v = v.length > 10 ? v.substr(0, 10) : v
+      v =
+        v.length === 10
+          ? `(${v.substr(0, 3)}) ${v.substr(3, 3)}-${v.substr(6, 4)}`
+          : v
+    }
+    return v
+  },
+  email: (v: string) => {
+    const [user, host] = v.toLowerCase().substring(0, 320).split(/@/)
+    return `${user.trim()}${host !== undefined ? `@${host.trim()}` : ""}`
+  },
+}
