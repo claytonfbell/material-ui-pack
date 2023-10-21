34dd98995781c5364d6fb2f1974d450c7a3fae27
Clay Bell <clay.bell@etix.com>
2023-10-21 11:56:47 -0700

    fix: added dayjs advancedFormat plugin

--- src/dayjs.ts
+++ src/dayjs.ts
@@ -2,9 +2,11 @@ import dayjs from "dayjs"
 import timezone from "dayjs/plugin/timezone"
 import utc from "dayjs/plugin/utc"
 import relativeTime from "dayjs/plugin/relativeTime"
+import advanced from "dayjs/plugin/advancedFormat"
 
 dayjs.extend(utc)
 dayjs.extend(timezone)
 dayjs.extend(relativeTime)
+dayjs.extend(advanced)
 
 export default dayjs
